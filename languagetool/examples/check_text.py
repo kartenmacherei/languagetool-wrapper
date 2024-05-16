fea6ac022f5958cde184f74ef57d98463a500090
Martin Schmitt <mnschmit@users.noreply.github.com>
2024-05-16 08:53:09 +0200

    added more options to check text example

--- languagetool/examples/check_text.py
+++ languagetool/examples/check_text.py
@@ -16,7 +16,11 @@ if __name__ == "__main__":
     parser.add_argument("text")
     parser.add_argument("language")
     parser.add_argument("--dict_names", nargs="*", dest="dicts")
+    parser.add_argument("--enabled_rules", nargs="*")
     parser.add_argument("--disabled_rules", nargs="*")
+    parser.add_argument("--enabled_categories", nargs="*")
+    parser.add_argument("--disabled_categories", nargs="*")
+    parser.add_argument("--enabled_only", action="store_true")
     args = parser.parse_args()
 
     logger.info("Using dictionaries %s", str(args.dicts))
@@ -24,8 +28,16 @@ if __name__ == "__main__":
     checker = LanguageToolChecker()
 
     kwargs = {}
+    if args.enabled_rules:
+        kwargs["enabledRules"] = ",".join(args.enabled_rules)
     if args.disabled_rules:
         kwargs["disabledRules"] = ",".join(args.disabled_rules)
+    if args.enabled_categories:
+        kwargs["enabledCategories"] = ",".join(args.enabled_categories)
+    if args.disabled_categories:
+        kwargs["disabledCategories"] = ",".join(args.disabled_categories)
+    if args.enabled_only:
+        kwargs["enabledOnly"] = "true"
 
     matches = checker.check_text(
         args.text, args.language, dict_names=args.dicts, **kwargs
